f912a8ca8e1094b1e0f1935654bec1576d587b0e
krishnafrx <historyhuro@gmail.com>
2024-12-29 23:17:23 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
+    OWNER_ID = "5457689317"
+    sudo_users = "5457689317"
+    GROUP_ID = -1001875294969
     TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
     mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "team_cyclone"
+    UPDATE_CHAT = "team_cyclone"
+    BOT_USERNAME = "Catch_your_waifu_bot
+    CHARA_CHANNEL_ID = "-1001875294969"
+    api_id = 24302435
+    api_hash = "a814be3b1816d5a7dce5f1f75aa6d28d"
 
     
 class Production(Config):
